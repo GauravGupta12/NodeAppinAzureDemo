278196c29b75c6557e67957eb54fb28ea6f49429
Gaurav Gupta <ggupta2@mail.depaul.edu>
2018-07-01 20:43:30 -0500

    changed port to 3000

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var corOptions = {
     optionsSuccessStatus: 200
 };
 app.use(cors(corOptions));
-app.listen(port,() =>{
+app.listen(3000,() =>{
     console.log('Server started :)');
 });
 app.route('/api/cats').get((req, res) =>{
